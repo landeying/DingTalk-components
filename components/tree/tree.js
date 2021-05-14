13ee61b315f356fa36fa8dde629783aa28bc5fb8
liujing <liujing@zkcmg.com>
2021-05-14 16:43:37 +0800

    添加注释

--- components/tree/tree.js
+++ components/tree/tree.js
@@ -2,15 +2,13 @@ Component({
   mixins: [],
   data: {},
   props: {
-    list: Object,
-    icon: '',
-    direction: 'row',
-    pid: 0,
-    cid: 0,
-    onSelect: () => {
-    },
-    onChildSelect: () => {
-    }
+    list: Object, // 列表数据
+    icon: '', // 节点图标
+    direction: 'row', // 排列方式，row: 横向排列 column：纵向排列
+    pid: 0, // 父节点id
+    cid: 0, // 子节点id
+    onSelect: () => { }, //  根节点选择
+    onChildSelect: () => { } // 其他节点选择
   },
   didMount() {
   },
