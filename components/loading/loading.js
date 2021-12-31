c49b029c90020cdea7d22162857b31bc7b10a16e
liujing <liujing@zkcmg.com>
2021-12-31 16:32:20 +0800

    优化loading显示

--- components/loading/loading.js
+++ components/loading/loading.js
@@ -1,13 +1,24 @@
 Component({
   mixins: [],
-  data: {},
+  data: {
+    height: 0
+  },
   props: {
     show: true, // 显示loading
     className: '', // loading类名
     content: '加载中...', // loading文字
   },
-  didMount() {},
-  didUpdate() {},
-  didUnmount() {},
+  didMount() {
+    dd.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          height: res.windowHeight
+        })
+        console.log(this.data.height)
+      }
+    })
+  },
+  didUpdate() { },
+  didUnmount() { },
   methods: {},
 });
